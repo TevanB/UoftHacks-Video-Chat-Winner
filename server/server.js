2594b563bd412d5a4093befe262b48f4b9c16ab9
TevanB <tevan.b@gmail.com>
2021-02-20 17:59:05 -0500

    node express server setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-print("BOOM");
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/getuser', (req, res) => {
+  res.send('me user!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
